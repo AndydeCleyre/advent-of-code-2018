c17b5e5774ad39f7a792fde40000c63462bd5339
Andy Kluger <andykluger@gmail.com>
2018-12-04 14:47:30 -0500

    use cycle rather than while

--- 02/solve.py
+++ 02/solve.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+from itertools import cycle
+
 if __name__ == '__main__':
     freq = 0
     history = set((0,))
-    done = False
-    while not done:
-        with open('input.txt', 'r') as numfile:
-            for line in numfile:
-                freq += int(line)
-                if freq in history:
-                    print(freq)
-                    done = True
-                    break
-                else:
-                    history.add(freq)
+    with open('input.txt', 'r') as numfile:
+        for line in cycle(numfile):
+            freq += int(line)
+            if freq in history:
+                print(freq)
+                break
+            else:
+                history.add(freq)
