c7c0160092c69bb49eb10709fc9880deb7da16d4
Andy Kluger <andykluger@gmail.com>
2018-12-04 14:48:42 -0500

    drop the __main__ check; useless in this context

--- 01/solve.py
+++ 01/solve.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
-if __name__ == '__main__':
-    with open('input.txt', 'r') as numfile:
-        print(sum(map(int, numfile)))
+with open('input.txt', 'r') as numfile:
+    print(sum(map(int, numfile)))
--- 02/solve.py
+++ 02/solve.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 from itertools import cycle
 
-if __name__ == '__main__':
-    freq = 0
-    history = set((0,))
-    with open('input.txt', 'r') as numfile:
-        for line in cycle(numfile):
-            freq += int(line)
-            if freq in history:
-                print(freq)
-                break
-            else:
-                history.add(freq)
+freq = 0
+history = set((0,))
+with open('input.txt', 'r') as numfile:
+    for line in cycle(numfile):
+        freq += int(line)
+        if freq in history:
+            print(freq)
+            break
+        else:
+            history.add(freq)
